de156d42037796a5097fbb9284e680429c12ff30
ahmet-lab34 <ahmetcuma321@gmail.com>
2026-01-12 22:14:46 +0300

    Fixed

--- Assets/Scripts/Player/PlayerScript.cs
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -1,4 +1,3 @@
-using System.Numerics;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,13 +7,10 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 using System;
 
-
 public class PlayerScript : MonoBehaviour
 {
-    public GameObject ESC_Menu;
     private AllPlayerAudio PlayerAudio;
     private GroundCHK GroundCheck;
-    private UIScript UIScript;
     private Rigidbody2D rb;
 
     public Animator animator;
@@ -22,8 +18,9 @@ public class PlayerScript : MonoBehaviour
     private InputAction Sprint;
     private InputAction CrouchAction;
     private InputAction jumpAction;
-    private InputAction ESC_menuAction ;
+
     [SerializeField] private bool IsCrouching = false;
+    public bool IsCrouchingPublic => IsCrouching;
 
     [HideInInspector] public float Horizontal;
     [SerializeField] private bool DoubleJump = true;
@@ -31,13 +28,11 @@ public class PlayerScript : MonoBehaviour
     private float dashCounter;
     [HideInInspector] public float dashCoolcounter;
 
-
     public bool IsFacingRight;
 
     [SerializeField] private PlayerUIScript PlayerUIScript;
 
     [SerializeField] private bool isTouchingWall = false;
-    //[SerializeField] private bool isWallJumping = false;
     [SerializeField] private bool nearTheWall = false;
     [SerializeField] private int wallSide;
     private bool LongJumpRotationChecker = true;
@@ -48,6 +43,25 @@ public class PlayerScript : MonoBehaviour
 
     public Numerics playerNumbers = new Numerics();
     private float movementDashSpeedBuffer;
+
+    [SerializeField] private ParticleSystem upsideDownGroundSparks;
+    [SerializeField] private Transform sparksPivot;
+
+    private bool isUpsideDown;
+    private bool sparksPlaying;
+
+    [SerializeField] private float sparksXMin = 1.5f;
+    [SerializeField] private float sparksXMax = 3.0f;
+
+    [SerializeField] private float sparksAirSpeedMin = 1.5f;
+    [SerializeField] private float sparksAirSpeedMax = 3.0f;
+
+    [SerializeField] private float sparksBlendSpeed = 14f;
+    [SerializeField] private float movementDeadZone = 0.15f;
+
+    private Vector2 currentDir;
+    private float currentSpeedMin;
+    private float currentSpeedMax;
 
     [Serializable]
     public class PlayerStats
@@ -64,12 +78,14 @@ public class PlayerScript : MonoBehaviour
         public float wallJumpYAxis;
         public float wallJumpXAxis;
     }
+
     [Serializable]
     public class Numerics
     {
         public int playerHealth = 3;
         public int BirdCount = 0;
     }
+
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -80,57 +96,58 @@ public class PlayerScript : MonoBehaviour
         PlayerAudio = FindFirstObjectByType<AllPlayerAudio>();
         GroundCheck = FindFirstObjectByType<GroundCHK>();
         PlayerUIScript = FindFirstObjectByType<PlayerUIScript>();
-        UIScript = FindAnyObjectByType<UIScript>();
 
         Sprint = input.actions.FindAction("Sprint");
         CrouchAction = input.actions.FindAction("Crouch");
         jumpAction = input.actions.FindAction("Jump");
-        ESC_menuAction = input.actions.FindAction("ESC_Button");
 
         Time.timeScale = 1f;
+
+        currentSpeedMin = sparksXMin;
+        currentSpeedMax = sparksXMax;
+        currentDir = IsFacingRight ? Vector2.left : Vector2.right;
     }
+
     void Start()
     {
-
         normalHeight = transform.localScale;
-
-        //Actions
-
-
-
+        originalStats.activeMoveSpeed = movingStats.activeMoveSpeed;
+        originalStats.jumpHeight = movingStats.jumpHeight;
+        originalStats.DoubleJumpHeight = movingStats.DoubleJumpHeight;
+        originalStats.dashSpeed = movingStats.dashSpeed;
+        originalStats.dashCooldown = movingStats.dashCooldown;
+        originalStats.slideSpeed = movingStats.slideSpeed;
+        originalStats.gravityScale = movingStats.gravityScale;
+        originalStats.wallJumpYAxis = movingStats.wallJumpYAxis;
+        originalStats.wallJumpXAxis = movingStats.wallJumpXAxis;
     }
+
     private void FixedUpdate()
     {
         rb.linearVelocity = new UnityEngine.Vector2(Horizontal * movingStats.activeMoveSpeed, rb.linearVelocity.y);
     }
+
     void Update()
     {
         Horizontal = Input.GetAxisRaw("Horizontal");
-        //wall slide logic
+
         if (isTouchingWall)
         {
             rb.linearVelocity = new UnityEngine.Vector2(Horizontal * movingStats.activeMoveSpeed, -movingStats.slideSpeed);
         }
-        //Sound Call
-        PlayerAudio.walkingSound();
 
-        //wall jump script
+        PlayerAudio.walkingSound();
 
         if (nearTheWall && jumpAction.triggered)
         {
-            //QUESTION MARK !!!!!!!!!
             UnityEngine.Vector2 jumpDirection = new UnityEngine.Vector2(rb.linearVelocity.x, movingStats.wallJumpYAxis);
-            //!!!!!!!!!
-
             rb.linearVelocity = jumpDirection;
             nearTheWall = !nearTheWall;
         }
-        //running animation
 
         animator.SetBool("IsRunning", Horizontal > 0 || Horizontal < 0);
         animator.SetFloat("yVelocity", rb.linearVelocity.y);
 
-        //Resetting the double jump
         if (GroundCheck.Grounded)
         {
             animator.SetBool("IsJumping", false);
@@ -140,7 +157,6 @@ public class PlayerScript : MonoBehaviour
                 DoubleJump = false;
             }
         }
-        //jump and double jump script
 
         if (jumpAction.triggered && !IsCrouching)
         {
@@ -157,23 +173,21 @@ public class PlayerScript : MonoBehaviour
                 PlayerAudio.Jumping();
             }
         }
+
         if (!GroundCheck.Grounded)
         {
             animator.SetBool("IsJumping", true);
         }
-        //long jump script
 
-        if (jumpAction.triggered && (LongJumpRotationChecker?
-        rb.linearVelocity.y > 0f : rb.linearVelocity.y < 0f))
+        if (jumpAction.triggered && (LongJumpRotationChecker ? rb.linearVelocity.y > 0f : rb.linearVelocity.y < 0f))
         {
             rb.linearVelocity = new UnityEngine.Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
         }
-        //Flipping the character
+
         if ((IsFacingRight && Horizontal < 0f) || (!IsFacingRight && Horizontal > 0f))
         {
             Flip();
         }
-        //dash script
 
         if (Sprint.triggered && !IsCrouching)
         {
@@ -185,6 +199,7 @@ public class PlayerScript : MonoBehaviour
                 animator.SetBool("IsDashing", true);
             }
         }
+
         if (dashCounter > 0)
         {
             dashCounter -= Time.deltaTime;
@@ -195,25 +210,121 @@ public class PlayerScript : MonoBehaviour
                 animator.SetBool("IsDashing", false);
             }
         }
+
         if (dashCoolcounter > 0)
         {
             dashCoolcounter -= Time.deltaTime;
         }
-        //Crouch Script
-        CrouchAction.performed += ctx => Crouch();
-        //jumpAction.performed += ctx => StandUp();
+
         if (jumpAction.triggered && IsCrouching)
         {
             StandUp();
         }
 
-        //ESC Button
-        if (ESC_menuAction.triggered)
+        UpdateUpsideDownSparks();
+    }
+
+    private void UpdateUpsideDownSparks()
+    {
+        if (upsideDownGroundSparks == null || GroundCheck == null || rb == null) return;
+
+        var main = upsideDownGroundSparks.main;
+        main.simulationSpace = ParticleSystemSimulationSpace.World;
+
+        var v = upsideDownGroundSparks.velocityOverLifetime;
+        v.enabled = true;
+        v.space = ParticleSystemSimulationSpace.World;
+
+        bool shouldPlay = isUpsideDown;
+
+        bool grounded = GroundCheck.Grounded;
+
+        Vector2 targetDir = currentDir;
+        float targetSpeedMin = currentSpeedMin;
+        float targetSpeedMax = currentSpeedMax;
+
+        if (grounded)
         {
-            ESC_Menu.SetActive(true);
-            Time.timeScale = 0f;
+            targetSpeedMin = sparksXMin;
+            targetSpeedMax = sparksXMax;
+            targetDir = IsFacingRight ? Vector2.left : Vector2.right;
+        }
+        else
+        {
+            targetSpeedMin = sparksAirSpeedMin;
+            targetSpeedMax = sparksAirSpeedMax;
+
+            Vector2 vel = rb.linearVelocity;
+
+            if (vel.sqrMagnitude < movementDeadZone * movementDeadZone)
+            {
+                targetDir = currentDir;
+            }
+            else
+            {
+                targetDir = (-vel).normalized;
+            }
+        }
+
+        float t = 1f - Mathf.Exp(-sparksBlendSpeed * Time.deltaTime);
+
+        currentDir = Vector2.Lerp(currentDir, targetDir, t);
+        float mag = currentDir.magnitude;
+        if (mag > 0.0001f) currentDir /= mag;
+
+        currentSpeedMin = Mathf.Lerp(currentSpeedMin, targetSpeedMin, t);
+        currentSpeedMax = Mathf.Lerp(currentSpeedMax, targetSpeedMax, t);
+
+        float minS = Mathf.Min(currentSpeedMin, currentSpeedMax);
+        float maxS = Mathf.Max(currentSpeedMin, currentSpeedMax);
+
+        v.x = new ParticleSystem.MinMaxCurve(currentDir.x * minS, currentDir.x * maxS);
+        v.y = new ParticleSystem.MinMaxCurve(currentDir.y * minS, currentDir.y * maxS);
+        v.z = new ParticleSystem.MinMaxCurve(0f, 0f);
+
+        if (shouldPlay)
+        {
+            if (!sparksPlaying)
+            {
+                upsideDownGroundSparks.Play(true);
+                sparksPlaying = true;
+            }
+        }
+        else
+        {
+            if (sparksPlaying || upsideDownGroundSparks.isPlaying)
+            {
+                upsideDownGroundSparks.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+                sparksPlaying = false;
+            }
         }
     }
+
+    private void ApplySparksFacing()
+    {
+        if (sparksPivot == null) return;
+        sparksPivot.localRotation = Quaternion.Euler(0f, 0f, IsFacingRight ? 0f : 180f);
+    }
+
+    public void SetUpsideDown(bool value)
+    {
+        isUpsideDown = value;
+
+        if (!isUpsideDown)
+        {
+            if (upsideDownGroundSparks != null)
+                upsideDownGroundSparks.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+
+            sparksPlaying = false;
+        }
+    }
+
+
+    public void ToggleUpsideDown()
+    {
+        isUpsideDown = !isUpsideDown;
+    }
+
     public void FlipJumpValues()
     {
         movingStats.jumpHeight = -movingStats.jumpHeight;
@@ -222,6 +333,7 @@ public class PlayerScript : MonoBehaviour
         movingStats.slideSpeed = -movingStats.slideSpeed;
         LongJumpRotationChecker = !LongJumpRotationChecker;
     }
+
     private void OnDestroy()
     {
         if (!LongJumpRotationChecker)
@@ -230,6 +342,7 @@ public class PlayerScript : MonoBehaviour
             LongJumpRotationChecker = !LongJumpRotationChecker;
         }
     }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Wall"))
@@ -239,6 +352,7 @@ public class PlayerScript : MonoBehaviour
             Debug.Log("I'm near the wall !");
         }
     }
+
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.CompareTag("Wall"))
@@ -248,6 +362,7 @@ public class PlayerScript : MonoBehaviour
             Debug.Log("Far from the wall !");
         }
     }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
@@ -266,6 +381,7 @@ public class PlayerScript : MonoBehaviour
             }
         }
     }
+
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Wall"))
@@ -277,15 +393,27 @@ public class PlayerScript : MonoBehaviour
             wallSide = 0;
         }
     }
-    //Crouch Script Logic
-    
+
     private void OnEnable()
     {
         jumpAction.Enable();
+        CrouchAction.Enable();
+        CrouchAction.performed += OnCrouchPerformed;
     }
+
     private void OnDisable()
     {
+        if (CrouchAction != null)
+        {
+            CrouchAction.performed -= OnCrouchPerformed;
+            CrouchAction.Disable();
+        }
         jumpAction.Disable();
+    }
+
+    private void OnCrouchPerformed(InputAction.CallbackContext ctx)
+    {
+        Crouch();
     }
 
     private void Crouch()
@@ -299,12 +427,13 @@ public class PlayerScript : MonoBehaviour
         }
         else if (!IsCrouching && GroundCheck.Grounded)
         {
-            movingStats.activeMoveSpeed *= 0.45f;
+            movingStats.activeMoveSpeed = originalStats.activeMoveSpeed * 0.45f;
             transform.localScale = new UnityEngine.Vector2(transform.localScale.x, normalHeight.y * 0.9f);
             IsCrouching = true;
             animator.SetBool("IsCrouching", true);
         }
     }
+
     private void StandUp()
     {
         transform.localScale = new UnityEngine.Vector2(transform.localScale.x, 1f);
@@ -312,6 +441,7 @@ public class PlayerScript : MonoBehaviour
         IsCrouching = false;
         animator.SetBool("IsCrouching", false);
     }
+
     public void GetHit()
     {
         playerNumbers.playerHealth -= 1;
@@ -322,11 +452,19 @@ public class PlayerScript : MonoBehaviour
         }
         Debug.Log("Player Got Hit !");
     }
+
+    public void Heal(int amount)
+    {
+        if (amount <= 0) return;
+
+        playerNumbers.playerHealth += amount;
+        Debug.Log("Player healed +" + amount + ". HP now: " + playerNumbers.playerHealth);
+    }
+
     public void DieFromSpikes()
     {
         PlayerUIScript.Die();
     }
-    
 
     private void Flip()
     {
@@ -335,6 +473,7 @@ public class PlayerScript : MonoBehaviour
         localScale.x *= -1f;
         transform.localScale = localScale;
     }
+
     public void CarryingaBird()
     {
         movingStats.activeMoveSpeed *= 0.85f;
@@ -346,21 +485,15 @@ public class PlayerScript : MonoBehaviour
         playerNumbers.BirdCount += 1;
         Debug.Log("I have " + playerNumbers.BirdCount + " birds !");
     }
+
     public void RidingaBird()
     {
-        movingStats.activeMoveSpeed = originalStats.activeMoveSpeed;// * (1f + BirdCount * 0.15f);
-
-        movingStats.jumpHeight = originalStats.jumpHeight;// * (1f + BirdCount * 0.15f);
-
-        movingStats.dashSpeed = originalStats.dashSpeed;// * (1f + BirdCount * 0.15f);
-
-        movingStats.dashCooldown = originalStats.dashCooldown;// * (1f - BirdCount * 0.15f);
-
-        movingStats.slideSpeed = originalStats.slideSpeed;// * (1f + BirdCount * 0.2f);
-
-        movingStats.gravityScale = originalStats.gravityScale;// * (1f + BirdCount * 0.2f);
-
-
+        movingStats.activeMoveSpeed = originalStats.activeMoveSpeed;
+        movingStats.jumpHeight = originalStats.jumpHeight;
+        movingStats.dashSpeed = originalStats.dashSpeed;
+        movingStats.dashCooldown = originalStats.dashCooldown;
+        movingStats.slideSpeed = originalStats.slideSpeed;
+        movingStats.gravityScale = originalStats.gravityScale;
 
         playerNumbers.BirdCount = 0;
         Debug.Log("I Don't have any birds anymore :(");
